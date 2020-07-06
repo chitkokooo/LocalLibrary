ba45f969c183053bf9a1d76abb29637cbe4f4d5b
Chit Ko Ko Oo <chitkokooo.cu@gmail.com>
2020-07-06 22:57:36 +0630

    add auth urls

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
