c5abbb3cf09728bcf1164e62382fdfc1e1a3fb54
Chit Ko Ko Oo <chitkokooo.cu@gmail.com>
2020-07-06 16:39:27 +0630

    add virtualenv files

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\TEMPLATES\Project\Learning Python\django\LocalLibrary\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
