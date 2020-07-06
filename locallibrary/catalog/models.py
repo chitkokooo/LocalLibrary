74c3ebaff4fe4c95e7bfb80baefab354dbfb2a36
Chit Ko Ko Oo <chitkokooo.cu@gmail.com>
2020-07-06 15:51:45 +0630

    add Author class to catalog model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -85,3 +85,24 @@ class BookInstance(models.Model):
 	def __str__(self):
 		"""String for representing the Model object."""
 		return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+	"""Model representing an author."""
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	date_of_birth = models.DateField(null=True, blank=True)
+	date_of_death = models.DateField('Died', null=True, blank=True)
+
+	class Meta:
+		ordering = ['last_name', 'first_name']
+
+	def get_absolute_url(self):
+		"""Returns the url to access a particular author instance."""
+		return reverse('author-detail', args=[str(self.id)])
+
+	def __str__(self):
+		"""String for representation the Model object."""
+		return f'{self.last_name}, {self.first_name}'
+
+
