4f398a1e335979cc6562785ec004eaeaff5f3e23
Chit Ko Ko Oo <chitkokooo.cu@gmail.com>
2020-07-07 14:25:15 +0630

    add can_mark_returned permission to bookinstance model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -99,6 +99,7 @@ class BookInstance(models.Model):
 
 	class Meta:
 		ordering = ['due_back']
+		permissions = (('can_mark_returned', "Set book as returned"),)
 
 	def __str__(self):
 		"""String for representing the Model object."""
