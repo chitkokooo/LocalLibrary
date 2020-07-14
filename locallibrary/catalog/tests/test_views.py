e6da523057ce6e8d88db980fd553c505a79b694a
Chit Ko Ko Oo <chitkokooo.cu@gmail.com>
2020-07-14 14:10:00 +0630

    renew book instance test class

--- locallibrary/catalog/tests/test_views.py
+++ locallibrary/catalog/tests/test_views.py
@@ -1,10 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, genre_objects_or_404
 from django.utils import timezone
-from django.contrib.auth.models import User  # Required to assign User as a borrower
+# Required to assign User as a borrower
+# Required to grant the permission needed to set a book as returned.
+from django.contrib.auth.models import User, Permission
 import datetime
+import uuid
 
 from catalog.models import Author, BookInstance, Book, Genre
+from catalog.forms import RenewBookForm
 
 
 class AuthorListViewTest(TestCase):
@@ -159,4 +164,86 @@ class LoanedBookInstancesByUserListViewTest(TestCase):
 					self.assertTrue(last_date <= book.due_back)
 					last_date = book.due_back
 
-				
+
+""" Testing views with form """
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+	"""View function for renewing a specific BookInstance by librarian."""
+	book_instance = genre_objects_or_404(BookInstance, pk=pk)
+
+	# If this is a POST request then process the Form data
+	if request.method == 'POST':
+		# Create a form instance and populate it with data from the request (binding):
+		book_renewal_form = RenewBookForm(request.POST)
+
+		# Check if the form is valid:
+		if form.is_valid():
+			# process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+			book_instance.due_back = form.cleaned_data['renew_date']
+			book_instance.save()
+
+			# redirect to a new URL:
+			return HttpResponseRedirect(reverse('all-borrowed'))
+
+	# If this is a GET (or any other method) create the default form
+else:
+	proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+	book_renewal_form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+context = {
+	'book_renewal_form': book_renewal_form,
+	'book_instance': book_instance,
+}
+
+return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class RenewBookInstanceViewTest(TestCase):
+	def setUp(self):
+		# Create a user
+		test_user1 = User.objects.create_user(username="testuser1", password="1X<ISRUkw+tuK")
+		test_user2 = User.objects.create_user(username="testuser2", password="2HJ1vRV0Z&3iD")
+		test_user1.save()
+		test_user2.save()
+
+		permission = Permission.objects.get(name="Set book as returned")
+		test_user2.user_permissions.add(permission)
+		test_user2.save()
+
+		# Create a book
+		test_author = Author.objects.create(first_name="John", last_name="Smith")
+		test_genre = Genre.objects.create(name="Fantasy")
+		test_language = Language.objects.create(name='English')
+		test_book = Book.objects.create(
+			title = "Book Title",
+			summary = "My book summary",
+			isbn = 'ABCDEFG',
+			author = test_author,
+			# language = test_language, 
+		)
+
+		# Create genre as a post-step
+		genre_objects_for_book = Genre.objects.all()
+		test_book.genre.set(genre_objects_for_book)  # Direct assignment of many-to-many types not allowed.
+		test_book.save()
+
+		# Create a BookInstance object for test_user1
+		return_date = datetime.date.today() + datetime.timedelta(days=5)
+		self.test_bookinstance1 = BookInstance.objects.create(
+			book = test_book,
+			imprint = 'Unlikely Imprint, 2016',
+			due_back = return_date,
+			borrower = test_user1,
+			status = 'o',
+		)
+
+		# Create a BookInstance object for test_user2
+		return_date = datetime.date.today() + datetime.timedelta(days=5)
+		self.test_bookinstance2 = BookInstance.object.create(
+			book = test_book,
+			imprint = 'Unlikely Imprint, 2016',
+			due_back = return_date,
+			borrower = test_user2,
+			status = 'o'
+		)
